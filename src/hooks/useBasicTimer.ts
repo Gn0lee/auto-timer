18d1d5f2521f2001961ff2a52926a8aa2e1d1776
이진호 <93526602+Gn0lee@users.noreply.github.com>
2024-01-25 21:43:26 +0900

    기본 타이머 시간 보정 삭제 (#11)

--- src/hooks/useBasicTimer.ts
+++ src/hooks/useBasicTimer.ts
@@ -1,39 +1,13 @@
-import { useCallback, useMemo, useEffect } from 'react';
-import { AppState, AppStateStatus } from 'react-native';
+import { useCallback, useMemo } from 'react';
 
-import { useAppDispatch, useAppSelector } from '@store/redux';
-import { increment, reset, setLastTimeBackground, updateElapsedTime } from '@store/basicTimerSlice';
+import { useAppDispatch } from '@store/redux';
+import { increment, reset } from '@store/basicTimerSlice';
 import Timer from '@class/Timer';
 
 export default function useBasicTimer() {
   const dispatch = useAppDispatch();
 
-  const { mode } = useAppSelector((state) => state.timer);
-
   const timer = useMemo(() => new Timer(999), []);
-
-  useEffect(() => {
-    const handleAppStateChange = (nextState: AppStateStatus) => {
-      if (nextState === 'active' && mode === 'running') {
-        timer.restart(() => {
-          dispatch(updateElapsedTime());
-        });
-        return;
-      }
-
-      if (nextState === 'inactive' || nextState === 'background') {
-        timer.pause(() => {
-          dispatch(setLastTimeBackground(new Date().getTime()));
-        });
-      }
-    };
-
-    const subscription = AppState.addEventListener('change', handleAppStateChange);
-
-    return () => {
-      subscription.remove();
-    };
-  }, [dispatch, timer, mode]);
 
   const start = useCallback(() => {
     timer.start(() => {
